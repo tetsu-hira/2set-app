4c01fb4ffac5a5a5dff3519d7ab20a30dbc46701
tetsu-hira <svc.snowdrop@gmail.com>
2022-09-04 23:40:08 +0900

    styled-components成功

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Icon from './image/search.png';
 
+const HeaderBlock = styled.div`
+  height: 48px;
+  background-color: #4d80b0;
+  width: 100%;
+  display: flex;
+  z-index: 100;
+  min-width: 960px;
+  position: relative;
+`;
+
 const Header: React.FC = () => {
   return (
-    <div className='Header'>
-      <div className='HeaderContainer'>
-        <div className='WebTitle'>2setMatch</div>
-        {/* <ul className='HeaderLink'>
-          <li className='HeaderLink__item'>
-            <Link to='/' className='HeaderLink__text'>
-              Home
-            </Link>
-          </li>
-          <li className='HeaderLink__item'>
-            <Link to='/top' className='HeaderLink__text'>
-              TOP
-            </Link>
-          </li>
-          <li className='HeaderLink__item'>
-            <Link to='/test' className='HeaderLink__text'>
-              TEST
-            </Link>
-          </li>
-        </ul> */}
-        <div className='HeaderRight'>
-          <div className='search'>
-            <img className='icon' src={Icon} alt='アイコン' />
+    <HeaderBlock>
+      <div className="HeaderContainer">
+        <div className="WebTitle">2setMatch</div>
+        <div className="HeaderRight">
+          <div className="search">
+            <img className="icon" src={Icon} alt="アイコン" />
           </div>
-          <div className='account'>
-            <a className='circle'>TH</a>
+          <div className="account">
+            <a className="circle">TH</a>
           </div>
         </div>
       </div>
-    </div>
+    </HeaderBlock>
   );
 };
 
---
+++ theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  space: ['0px', '4px', '8px', '16px', '24px', '32px'],
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+    prime: '#4d80b0',
+  },
+};
